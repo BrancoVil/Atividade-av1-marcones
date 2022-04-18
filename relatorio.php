1062b54005af65b6ff433123c7d401b4c8043ac7
Branco <joaovilarrrrr@hotmail.com>
2022-04-17 21:49:11 -0300

    Update relatorio.php

--- relatorio.php
+++ relatorio.php
@@ -18,17 +18,19 @@ $pdf->Cell(25,10,'Escola',1,0,'C');
 $pdf->Cell(25,10,'Qtd. Aluno',1,0,'C');
 $pdf->Cell(25,10,'Conteudo',1,0,'C');
 $pdf->Cell(25,10,'Professor',1,0,'C');
-$pdf->Cell(26,10,'Data de Visita',1,1,'C');
+$pdf->Cell(26,10,'Data de Visita',1,0,'C');
 
 while ($resultado = mysqli_fetch_array($result)) {
     $pdf->ln();
-    $pdf->Cell(40, 5, $resultado['id']);
-    $pdf->SetX(35);
-    $pdf->Cell(60, 5, $resultado['coordenador']);
-    $pdf->SetX(50);
-    $pdf->Cell(40, 5, $resultado['setor']);
-    $pdf->SetX(77);
-    $pdf->Cell(40, 5, $resultado['escola']);
+    $pdf->Cell(16, 10,$resultado['id'],1, 0,'C');
+    $pdf->Cell(25, 10,$resultado['coordenador'],1, 0,'C');
+    $pdf->Cell(25, 10, $resultado['setor'],1, 0,'C');
+    $pdf->Cell(25, 10, $resultado['escola'],1, 0,'C');
+    $pdf->Cell(25, 10, $resultado['quantidadedealunos'],1, 0,'C');
+    $pdf->Cell(25, 10, $resultado['conteudododia'],1, 0,'C');
+    $pdf->Cell(25, 10, $resultado['professor'],1, 0,'C');
+    $pdf->Cell(26, 10, $resultado['datavisita'],1, 0,'C');
+
 }
 
 $pdf->Output();
